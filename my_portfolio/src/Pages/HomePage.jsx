111893eaee02aab8057dd6e8c4060a9da2cbee0a
CharlieNtabo <charlientr7@gmail.com>
2025-05-20 23:51:57 +0300

    my beautiful portfolio using react & tailwindcss

---
+++ my_portfolio/src/Pages/HomePage.jsx
@@ -0,0 +1,18 @@
+import { StarBackground } from "../Component/StarBackground.jsx"
+import { ThemeToggle } from "../Component/ThemeToggle.jsx"
+
+export const Home = () => {
+    return <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
+
+        {/* Theme Toggle */}
+            <ThemeToggle />
+        {/* Background Effects */}
+            <StarBackground />
+        {/* Navbar */}
+
+        {/* Main content */}
+
+
+        {/* Footer */}
+    </div>
+}
